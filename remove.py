bfd4918900959473470179e38881e101bfd5e6d7
antema103 <antema103@gmail.com>
2024-11-25 21:36:12 +0300

    remove

---
+++ remove.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+
+def supprimer_dossier(chemin):
+    if os.path.isfile(chemin):
+        try:
+            os.remove(chemin)  # Supprime le dossier et son contenu
+            print(f"Dossier supprimé : {chemin}")
+        except Exception as e:
+            print(f"Erreur lors de la suppression : {e}")
+    else:
+        print(f"Le dossier n'existe pas : {chemin}")
+
+if __name__ == "__main__":
+    chemin_dossier = r"C:\Users\antema\Desktop\Scrapping\processed_elements.txt"  # Remplacez par le chemin du dossier à supprimer
+    supprimer_dossier(chemin_dossier)
