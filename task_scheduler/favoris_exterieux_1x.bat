3395aac403583860dc8e9bacb9a47bdab0e9843d
antema102 <antema103@gmail.com>
2024-12-25 19:54:26 +0300

    update bash

---
+++ task_scheduler/favoris_exterieux_1x.bat
@@ -0,0 +1,3 @@
+@echo off
+C:/python/python.exe "C:\Users\Administrator\Desktop\scrapping_aiscore\1xBet\Favoris_exterieur\1xBetAutomatiques.py"
+exit
---
+++ task_scheduler/update_but_totaux.bat
@@ -0,0 +1,3 @@
+@echo off
+C:/python/python.exe "C:\Users\Administrator\Desktop\scrapping_aiscore\1xBet\buts totaux stats\buts_totaux.py"
+exit
